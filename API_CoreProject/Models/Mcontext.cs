4be2f4e799decc71b7294e7ec64abe6350e4e028
mahdi hajian <mahdi.haj.1375@gmail.com>
2018-07-19 15:33:38 +0430

    add optional Fluant Api

--- API_CoreProject/Models/Mcontext.cs
+++ API_CoreProject/Models/Mcontext.cs
@@ -1,4 +1,5 @@
 ﻿using API_CoreProject.Models.CRUD_Angular;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -32,12 +33,37 @@ namespace API_CoreProject.Models
         protected override void OnModelCreating(ModelBuilder builder)
         {
 
-            // seed
+            #region Seed
             // وقتی که تیبل خالی باشد شروع میکند سید کردن و باید پارامتر آیدی که آیدنتیتی هم هست هم پاس شود
             builder.Entity<Company>().HasData(new Company(1, "microsoft", "a good company"));
+            #endregion
 
+            #region Option
             builder.Entity<Company>().HasIndex(c => c.Name).IsUnique();
             builder.Entity<Company>().Property(c => c.Description).IsRequired();
+            #endregion
+
+            #region Lazy Loading
+            builder.Entity<Company>().Property(date => date.FoundedData).HasDefaultValueSql("GETDATE()");
+            builder.Entity<Company>().Property(date => date.Name).HasDefaultValueSql("''");
+            builder.Entity<Company>().Property(date => date.Description).HasDefaultValueSql("''");
+            #endregion
+
+            #region Relationships
+            builder.Entity<Technology>().HasOne(b => b.Company).WithMany(b => b.Technologies).OnDelete(DeleteBehavior.Cascade);
+            #endregion
+
+            #region Change Name
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim");
+            builder.Entity<IdentityRole>().ToTable("Role");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+            // باید بجای ایدنتیتی یوزر اپلیکیشن یوزر بنویسیم و کلاس ان را بسازیم
+            builder.Entity<IdentityUser>().ToTable("User");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+            #endregion
+
         }
     }
 }
