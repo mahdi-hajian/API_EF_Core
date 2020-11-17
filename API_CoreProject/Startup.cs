c1d3fae214b0bfa6109a13a56aac6ebfbc37dd7b
mahdi hajian <mahdi.haj.1375@gmail.com>
2020-11-17 22:17:21 +0330

    add sqlite to setup

--- API_CoreProject/Startup.cs
+++ API_CoreProject/Startup.cs
@@ -26,8 +26,8 @@ namespace API_CoreProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString = Configuration.GetConnectionString("SampleData");
-            services.AddDbContext<Mcontext>(cfg => cfg.UseSqlServer(connectionString));
+            var connectionString = Configuration.GetConnectionString("SampleData"); //SampleData >> "sqlite"
+            services.AddDbContext<Mcontext>(cfg => cfg.UseSqlServer(connectionString)); //  UseSqlServer >> UseSqlite
 
             // ********************
             // Setup CORS
